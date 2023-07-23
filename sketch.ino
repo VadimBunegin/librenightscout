c760b4c3da518aa734f60c54a2972a9cec5979b8
VadimBunegin <61832909+VadimBunegin@users.noreply.github.com>
2023-07-23 20:04:19 +0500

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,19 +1,31 @@
-/*
-  Complete project details: https://RandomNerdTutorials.com/esp8266-nodemcu-https-requests/ 
-  Based on the BasicHTTPSClient.ino Created on: 20.08.2018 (ESP8266 examples)
-*/
- 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecureBearSSL.h>
+#include <AccelStepper.h>
+#include <Ticker.h>
 
-// Replace with your network credentials
 const char* ssid = "Beeline_5E79";
 const char* password = "92515952";
+String part01;
 
-String getValue(String data, char separator, int index)
-{
+#define IN1 5
+#define IN2 4
+#define IN3 14
+#define IN4 12
+
+// Настройки шагового двигателя
+AccelStepper stepper(AccelStepper::FULL4WIRE, IN1, IN2, IN3, IN4);
+bool shouldRotate = false;
+bool tickerEnabled = false;
+
+void IRAM_ATTR onTimerISR() {
+  shouldRotate = true;
+}
+
+Ticker timerISR;
+
+String getValue(String data, char separator, int index) {
   int found = 0;
   int strIndex[] = {0, -1};
   int maxIndex = data.length()-1;
@@ -25,19 +37,11 @@ String getValue(String data, char separator, int index)
         strIndex[1] = (i == maxIndex) ? i+1 : i;
     }
   }
-
   return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
 void setup() {
   Serial.begin(115200);
-  //Serial.setDebugOutput(true);
-
-  Serial.println();
-  Serial.println();
-  Serial.println();
-
-  //Connect to Wi-Fi
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi ..");
@@ -45,46 +49,55 @@ void setup() {
     Serial.print('.');
     delay(1000);
   }
+
+  // Настройка шагового двигателя
+  stepper.setMaxSpeed(1000.0); // Установка максимальной скорости в шагах в секунду
+  stepper.setAcceleration(1000.0); // Установка ускорения в шагах в секунду квадрат
+
+  // Настройка аппаратного прерывания таймера
+  timerISR.attach(2.0, onTimerISR); // Прерывание каждые 2 секунды
 }
 
 void loop() {
-  // wait for WiFi connection
   if ((WiFi.status() == WL_CONNECTED)) {
-
     std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
-
-    // Ignore SSL certificate validation
     client->setInsecure();
-    
-    //create an HTTPClient instance
+
     HTTPClient https;
-    
-    //Initializing an HTTPS communication using the secure client
-    Serial.print("[HTTPS] begin...\n");
-    if (https.begin(*client, "https://librenightscout.ru/api/v1/entries/sgv?count=1")) {  // HTTPS
-      Serial.print("[HTTPS] GET...\n");
-      // start connection and send HTTP header
+    if (https.begin(*client, "https://librenightscout.ru/api/v1/entries/sgv?count=1")) {
       int httpCode = https.GET();
-      // httpCode will be negative on error
       if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
         Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
-        // file found at server
         if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
           String payload = https.getString();
-          String part01 = getValue(payload,'\t',2);
+          part01 = getValue(payload, '\t', 2);
           Serial.println(part01);
+
+          // Проверяем условие и управляем флагом для вращения двигателя
+          int value = part01.toInt();
+          if (value > 270) {
+            tickerEnabled = true; // Включаем таймерное прерывание
+          } else {
+            tickerEnabled = false; // Выключаем таймерное прерывание
+          }
         }
       } else {
         Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
       }
-
       https.end();
     } else {
       Serial.printf("[HTTPS] Unable to connect\n");
     }
   }
-  Serial.println();
-  Serial.println("Waiting 2min before the next round...");
+
+  // Управление двигателем
+  if (shouldRotate && tickerEnabled) {
+    // Если должны вращать и флаг включен, то сделаем один шаг
+    shouldRotate = false;
+    stepper.move(1000); // Вращаем на 90 градусов (зависит от конкретного двигателя и механики)
+    stepper.runToPosition();
+  }
+
+  // Возможно, вам понадобится небольшая задержка, чтобы дать время другим задачам обрабатываться
   delay(5000);
 }
